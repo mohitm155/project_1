325d723f6cf970e34fd2e4dace1ce7641558da5e
umesh <umeshm155@gmail.com>
2022-04-19 11:37:27 +0530

    mohit_1_commit

--- ddd/src/ddd/umesh.java
+++ ddd/src/ddd/umesh.java
@@ -6,10 +6,13 @@ import java.util.List;
 public class umesh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		List<String> l1 = new ArrayList<>();
 		l1.add("umesh");
 		System.out.println(l1);
+		List<String> l2 = new ArrayList<>();
+		l2.add("Nilesh");
+		System.out.println(l1);
+		System.out.println(l2);
 	}
 
 }
